76c2432b439d0f8cd3882212e0ce00e213b9f58d
Gaurav Kapur <gauravkapur596@gmail.com>
2025-08-14 12:08:54 +0530

    added styled button component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,6 @@
 import AvatarCircle from "@/components/AvatarCircle";
 import NameDescription from "@/components/NameDescription";
-import TagPill from "./tagPill";
+import CallToActionButton from "./CallToActionButton";
 
 
 export default function Hero() {
@@ -12,15 +12,7 @@ export default function Hero() {
         name="Gaurav Kapur"
         description="Designing and Developing websites for bold startups, businesses"
       />
-
-      <div className="flex gap-3 mt-4">
-        <TagPill label="Design" />
-        <TagPill label="Framer" />
-      </div>
-
-      <button className="mt-6 px-6 py-2 bg-black text-white rounded-full shadow-lg hover:scale-105 transition">
-        Book a Call
-      </button>
+       <CallToActionButton label="Book a Call" />
     </section>
   );
 }
